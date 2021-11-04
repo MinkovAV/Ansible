78d87393e3be3cae5d2fdae6da0ca155368c280b
Minkov Aleksey <3403405@gmail.com>
2021-11-04 19:35:40 +0500

    много доработок

--- RUN_ME.sh
+++ RUN_ME.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 export ANSIBLE_CONFIG=$PWD/ansible.cfg
-ansible-playbook main.yml
+
+
+echo Please, inpup ssh port for remote connrction?
+read -p 'default [2022]: ' ssh_port
+ssh_port="${ssh_port:=2022}"
+
+
+ansible-playbook main.yml --extra-vars "ssh_port=$ssh_port"
 
